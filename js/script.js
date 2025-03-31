41fcb9260e611dbaac67263fe2ad42b3d45abdb4
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-03-31 15:10:19 +0000

    Salary Program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the salary.
+ */
+function hoursWorked() {
+  // input
+  let hoursWorked = parseFloat(document.getElementById('hours-worked').value);
+  let hourlyRate = parseFloat(document.getElementById('hourly-rate').value);
+    const TAX = 20.05;
+  
+
+  // process
+    let gross = hoursWorked * hourlyRate;
+    let govPay = gross * TAX / 100;
+    let net = gross - govPay;
+
+  // output
+    document.getElementById("gov-pay").innerHTML = "The Government Takes: " + gross.toFixed(2) + "$";
+    document.getElementById("net-pay").innerHTML = "Your net pay is: " + net.toFixed(2) + "$";
+}
